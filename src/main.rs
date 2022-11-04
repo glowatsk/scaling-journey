5ff0ab18c3b2635f4529d1b76204c264531d99c7
glowatsk <glowatsk@gmail.com>
2022-11-03 21:21:16 -0600

    Add in structured logging to main Add in telemetry package

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,19 @@ use std::net::TcpListener;
 use sqlx::PgPool;
 use straight2prod::configuration::get_configuration;
 use straight2prod::startup::run;
+use straight2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("straight2prod".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
